94e342d0c9f094f71c781d30d690aab1e81c69e4
Yannick Hutter <hutteryannick@gmail.com>
2024-03-24 19:27:54 +0100

    Initial commit

---
+++ nvim/lua/plugins/noirbuddy.lua
@@ -0,0 +1,13 @@
+return {
+    'jesseleite/nvim-noirbuddy',
+    dependencies = {
+        { 'tjdevries/colorbuddy.nvim' }
+    },
+    lazy = false,
+    priority = 1000,
+    opts = {
+        colors = {
+            primary = "#ce6561"
+        },
+    },
+}
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+
+    init = function()
+        require 'nvim-treesitter.configs'.setup {
+            ensure_installed = { "c", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            },
+        }
+    end
+}
+
+return { M }
