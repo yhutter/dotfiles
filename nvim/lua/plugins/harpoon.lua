b611b482979f1ebf9a787d4e30a0a000774d21f3
Yannick Hutter <hutteryannick@gmail.com>
2024-03-24 19:27:54 +0100

    Initial commit

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,9 @@
+return {
+    'ThePrimeagen/harpoon',
+    init = function()
+        local mark = require("harpoon.mark")
+        local ui = require("harpoon.ui")
+        vim.keymap.set("n", "<leader>a", mark.add_file)
+        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    end
+}
---
+++ nvim/lua/yhutter/set.lua
@@ -0,0 +1,19 @@
+vim.opt.guicursor = ""
+vim.g.mapleader = " "
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
