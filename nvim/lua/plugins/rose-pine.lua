8f99060746f97393959ff5dcff8be5985a6b9a6b
Yannick Hutter <hutteryannick@gmail.com>
2024-03-24 19:27:54 +0100

    Initial commit

---
+++ nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,12 @@
+-- lua/plugins/rose-pine.lua
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        vim.cmd("colorscheme rose-pine")
+
+        -- Transparent background
+        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    end
+}
