a96076814f0e40cac6160e4fd88a3f76e11f03ff
Yannick Hutter <hutteryannick@gmail.com>
2024-03-24 19:27:54 +0100

    Initial commit

---
+++ nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,12 @@
+return {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    config = function()
+        vim.cmd.colorscheme("gruvbox")
+
+        -- Transparent background
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
+}
